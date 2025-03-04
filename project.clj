5740a6cd5d6eeaeb8102c670c22fb81fe5b1fd90
Bill Hedworth <bill.hedworth@gmail.com>
2025-03-04 21:54:12 +0000

    version bump

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject org.clojars.bigsy/clj-http-stub "0.0.3"
+(defproject org.clojars.bigsy/clj-http-stub "0.1.3"
   :description "Helper for faking clj-http requests in testing"
   :url "https://github.com/Bigsy/clj-http-stub"
   :license {:name "MIT License"
             :url  "http://www.opensource.org/licenses/mit-license.php"
             :distribution :repo}
   :min-lein-version "2.0.0"
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :sign-releases false}]]
   :dependencies [[org.clojure/math.combinatorics "0.3.0"]
                  [robert/hooke "1.3.0"]
                  [clj-http "3.13.0"]
